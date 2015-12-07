681ea28bbe7687e0c04a39ca6c77cd5f2c8fc7e4
Christopher Waugh <cj.waugh@ECE-VDESK7-011.ad.ufl.edu>
2015-12-07 12:13:12 -0500

    going to lunch.  Just setting up 'database'

--- FinalProject.m
+++ FinalProject.m
@@ -1,15 +1,9 @@
 ShapeCount=24;
 VertexCount=3;
 
-Database = cell(5,2);
+Database = cell(5,1);
 for i = 1:5
-	path = strcat('MidtermFiles/0',int2str(i),'0',int2str(j),'.txt');
-	Database{i,1}=brokenEnroll(load(path),ShapeCount,VertexCount);
+	path = strcat('MidtermFiles/0',int2str(i),'01.txt');
+	Database{i,1}=brokenEnroll(load(path),ShapeCount,VertexCount,i);
 end
 
-
-for f1 = 1:5
-	for f2 = 1:5
-        
-    end
-end
